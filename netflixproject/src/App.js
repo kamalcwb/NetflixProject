5b39be2320240c882fc2a774bed79d17bc3b6ed0
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2020-09-02 15:20:49 -0300

    update

--- netflixproject/src/App.js
+++ netflixproject/src/App.js
@@ -59,8 +59,7 @@ export default () => {
         }
       </section>
       <footer>
-        Feito em Live (https://www.youtube.com/watch?v=tBweoUiMsDg) para estudo de react, todos os direitos das imagens são da Netflix.
-        Dados Extraidos de https://www.themoviedb.org/
+        Made with React, thanks to Netflix and themoviedb.org
       </footer>
 
 
--- netflixproject/src/tmdb.js
+++ netflixproject/src/tmdb.js
@@ -1,51 +1,72 @@
 const API_KEY = '65741d987bc73d9c5e06df02c1cf5a64'
 const API_BASE = 'https://api.themoviedb.org/3'
 
-const basicFetch = async (endpoint) => {
-    const req = await fetch(`${API_BASE}${endpoint}`)
-    const json = await req.json()
-    return json
+const basicFecth = async (endpoint) => {
+    return (await fetch(`${API_BASE}${endpoint}`)).json();
 }
 
 export default {
     getHomeList: async () => {
-        return [{
-            slug: 'originals',
-            title: 'Originais do Netflix',
-            itens: await basicFetch(`/discover/tv?with_network=213&language=pt-BRapi_key=${API_KEY}`)
-        },
-        {
-            slug: 'trending',
-            title: 'Recomendados para Você',
-            itens: await basicFetch(`/trending/all/week?language=pt-BRapi_key=${API_KEY}`)
-        }, {
-            slug: 'toprated',
-            title: 'Em alta',
-            itens: await basicFetch(`/movie/top_rated?language=pt-BRapi_key=${API_KEY}`)
-        }, {
-            slug: 'action',
-            title: 'Ação',
-            itens: await basicFetch(`/discover/movie?with_genres=28&language=pt-BRapi_key=${API_KEY}`)
-        }, {
-            slug: 'comedy',
-            title: 'Comédia',
-            itens: await basicFetch(`/discover/movie?with_genres=35&language=pt-BRapi_key=${API_KEY}`)
-        }, {
-            slug: 'horror',
-            title: 'Terror',
-            itens: await basicFetch(`/discover/movie?with_genres=27&language=pt-BRapi_key=${API_KEY}`)
-        }, {
-            slug: 'romance',
-            title: 'Romance',
-            itens: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BRapi_key=${API_KEY}`)
-        }, {
-            slug: 'documentary',
-            title: 'Documentários',
-            itens: await basicFetch(`/discover/movie?with_genres=99&language=pt-BRapi_key=${API_KEY}`)
-        }, {
-            slug: 'animation',
-            title: 'Animação',
-            itens: await basicFetch(`/discover/movie?with_genres=16&language=pt-BRapi_key=${API_KEY}`)
-        }]
+        return [
+            {
+                slug: 'originals',
+                title: "Originais do Netflix",
+                items: await basicFecth(`/discover/tv/?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'trending',
+                title: "Recomendados para Você",
+                items: await basicFecth(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'toprated',
+                title: "Em Alta",
+                items: await basicFecth(`/movie/top_rated?&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'action',
+                title: "Ação",
+                items: await basicFecth(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'comedy',
+                title: "Comédia",
+                items: await basicFecth(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'horror',
+                title: "Terror",
+                items: await basicFecth(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'romance',
+                title: "Romance",
+                items: await basicFecth(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'documentary',
+                title: "Documentários",
+                items: await basicFecth(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+            },
+        ]
+    },
+
+    getMovieInfo: async (movieId, type) => {
+        let info = {};
+        if (movieId) {
+            switch (type) {
+                case 'movei':
+                    info = await basicFecth(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                    break;
+                case 'tv':
+                    info = await basicFecth(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                    break;
+                default:
+                    info = null;
+                    break;
+            }
+        }
+
+        return info;
     }
 }
